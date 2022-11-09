ac93b4ea474e54e91f54a9c096a52f1a325b2e42
Murad <muradmustafayev03@gmail.com>
2022-11-10 01:09:40 +0400

    ...

--- cds.py
+++ cds.py
@@ -1,18 +1,50 @@
-# !/usr/bin/env python
 import cdsapi
 
 c = cdsapi.Client()
 
-c.retrieve('reanalysis-era5-complete', {
-    # Requests follow MARS syntax
-    # Keywords 'expver' and 'class' can be dropped. They are obsolete
-    # since their values are imposed by 'reanalysis-era5-complete'
-    'date': '2013-01-01',  # The hyphens can be omitted
-    'levelist': '1/10/100/137',  # 1 is top level, 137 the lowest model level in ERA5. Use '/' to separate values.
-    'levtype': 'ml',
-    'param': '130',  # Full information at https://apps.ecmwf.int/codes/grib/param-db/
-    # The native representation for temperature is spherical harmonics
-    'stream': 'oper',  # Denotes ERA5. Ensemble members are selected by 'enda'
-    'time': '00/to/23/by/6',  # You can drop :00:00 and use MARS shorthand notation, instead of '00/06/12/18'
-    'type': 'an',
-}, 'output')  # Output file; in this example containing fields in grib format. Adapt as you wish.
+c.retrieve(
+    'reanalysis-era5-land-monthly-means',
+    {
+        'product_type': 'monthly_averaged_reanalysis',
+        'variable': [
+            '2m_dewpoint_temperature', '2m_temperature', 'skin_temperature',
+            'soil_temperature_level_1', 'soil_temperature_level_2', 'soil_temperature_level_3',
+            'soil_temperature_level_4',
+        ],
+        'year': [
+            '1950', '1951', '1952',
+            '1953', '1954', '1955',
+            '1956', '1957', '1958',
+            '1959', '1960', '1961',
+            '1962', '1963', '1964',
+            '1965', '1966', '1967',
+            '1968', '1969', '1970',
+            '1971', '1972', '1973',
+            '1974', '1975', '1976',
+            '1977', '1978', '1979',
+            '1980', '1981', '1982',
+            '1983', '1984', '1985',
+            '1986', '1987', '1988',
+            '1989', '1990', '1991',
+            '1992', '1993', '1994',
+            '1995', '1996', '1997',
+            '1998', '1999', '2000',
+            '2001', '2002', '2003',
+            '2004', '2005', '2006',
+            '2007', '2008', '2009',
+            '2010', '2011', '2012',
+            '2013', '2014', '2015',
+            '2016', '2017', '2018',
+            '2019', '2020', '2021',
+            '2022',
+        ],
+        'month': [
+            '01', '02', '03',
+            '04', '05', '06',
+            '07', '08', '09',
+            '10', '11', '12',
+        ],
+        'time': '00:00',
+        'format': 'grib',
+    },
+    'download.grib')
